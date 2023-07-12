6840a9569574dfe66ccd4fb84c94df47e8be9ae4
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2023-07-12 20:44:03 +0300

    pyt-2 3. hafta 1.ders

---
+++ Pyt_2/3. Hafta/Degiskenler.py
@@ -0,0 +1,69 @@
+"""
+Değişkenler = variable var
+Değiken tipleri ve dönüşümleri
+Uygulama (Alan Çevre Hacim VKİ hesabı)
+input()
+Uygulama
+"""
+# Yorum satırı
+
+a = 15
+b = 48
+yorum = "python çok güzel bir dilmiş #lovepython"
+kullanici_adi = "greenrock"
+
+print(a)
+print(b)
+print(yorum)
+print(kullanici_adi)
+
+# Hatalı Değişken kullanımlar
+##2myvar = "John" 
+##my-var = "John"
+##my var = "John"
+##myvar! = "John"
+
+s1 = 5
+s2 = 3
+s3 = 4
+
+print(s1+s2+s3)
+
+s1,s2,s3 = 9,8,7
+
+print(s1+s2+s3)
+
+new_var = s1
+
+print(new_var)
+
+print(type(s1))
+print(type(yorum))
+
+
+my_int = 25
+my_float = 12.5
+my_string = "25.5"
+my_boolean = False
+
+a = float(my_string)
+print("string to float =>", a)
+print("float to int =>", int(a))
+
+my_int = 0
+my_float = 0.0
+my_string = " "
+my_boolean = True
+
+print("string to bool =>", bool(my_string))
+print("int to bool =>", bool(my_int))
+print("float to bool =>", bool(my_float))
+
+print("bool to string =>", str(my_boolean))
+print("bool to int =>", int(my_boolean))
+print("bool to float =>", float(my_boolean))
+
+
+
+
+ 
