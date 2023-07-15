effdbddd9ef417b11f6f7804f6eae2f43df5f3f7
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2023-07-15 18:44:39 +0300

    pyt-3 3. hafta 1.ders

---
+++ Pyt_3/3. Hafta/3. Hafta.py
@@ -0,0 +1,45 @@
+"""
+operatörler
+if-else-elif
+"""
+
+# aritmatiksel / matematiksel operatörler
+# atama operatörleri
+# ilişkisel operand
+# mantıksal operand
+# bellek operand
+# bitsel operand
+
+print("Aritmetiksel Operatörler" )
+
+a = """
+Operator	Name	                    Example
++	Addition	                    x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	                    x / y	
+%	Modulus	                    x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+"""
+print(a)
+
+print("Atama operatörleri")
+
+a = """
+Operator	Example	Same As	
+=	x = 5	x = 5	
++=	x += 3	x = x + 3	
+-=	x -= 3	x = x - 3	
+*=	x *= 3	x = x * 3	
+/=	x /= 3	x = x / 3	
+%=	x %= 3	x = x % 3	
+//=	x //= 3	x = x // 3	
+**=	x **= 3	x = x ** 3
+"""
+print(a)
+
+print(25 > 19)
+
+
+
