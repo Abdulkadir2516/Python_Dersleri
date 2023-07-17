edaf20c606cdd276e58e2869fdec3831cfb4c217
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2023-07-17 20:42:08 +0300

    pyt-1 4. hafta 1.ders

---
+++ Pyt_1/4. Hafta/4. Hafta.py
@@ -0,0 +1,91 @@
+##"""*Ödev:*
+##Kullanıcı boy ve kilosunu girdiğinde Beden Kitle Endeksine göre
+##kilolumu yoksa zayıfmı olduğunu hesaplayan python programını yazınız
+##"""
+##
+##kilo = float(input("Kilonuzu Giriniz:"))
+##boy = float(input("Boyunuzu Giriniz: (örn: 1.72)"))
+##
+##a = """
+##18, 5 kg/m² ‘nin altındaki sonuçlar: İdeal kilonun altında
+##18, 5 kg/m² ile 24, 9 kg/m² arasındaki sonuçlar: İdeal kiloda
+##25 kg/m² ile 29, 9 kg/m² arasındaki sonuçlar: İdeal kilonun üstünde
+##30 kg/m² ile 39, 9 kg/m² arasındaki sonuçlar: İdeal kilonun çok üstünde (obez)
+##40 kg/m² üzerindeki sonuçlar: İdeal kilonun çok üstünde (morbid obez)
+##"""
+##print(a)
+##print(kilo/(boy*boy))
+##
+##input()
+
+# Operatörler
+
+# 1 Matematiksel Aritmetiksel Operatörler (+-*/%**//)
+# 2 Atama Operatörleri
+# 3 İlişkisel Operatörler
+# 4 Mantıksal Operatörler
+# 5 Bitsel Operatörler
+# 6 Hafıza Operatörleri
+
+
+print("1 Matematiksel Aritmetiksel Operatörler")
+
+a = """
+Operator	Name	                    Example
++	Addition                        x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	                    x / y	
+%	Modulus	                    x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+
+"""
+print(a)
+
+print("2 Atama Operatörleri")
+
+print("""
+Operator	Example	Same As
+=	x = 5	x = 5	
++=	x += 3	x = x + 3	
+-=	x -= 3	x = x - 3	
+*=	x *= 3	x = x * 3	
+/=	x /= 3	x = x / 3	
+%=	x %= 3	x = x % 3	
+//=	x //= 3	x = x // 3	
+**=	x **= 3	x = x ** 3
+""")
+
+##x = 2
+##print(x)
+##x += 5
+##print(x)
+##x *= 3
+##print(x)
+##x %=2
+##print(x)
+##x **= 182
+##print(x)
+##x -= 3
+##print(x)
+##x **= 5
+##print(x)
+##x //= 5
+##print(x)
+
+print("3 İlişkisel Karşılaştırma Operatörler")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
