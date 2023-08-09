ea04c81034ae89547c6ac94122e8f1a5fa265ee9
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2023-08-09 21:32:03 +0300

    pyt-2 7. hafta 2. Ders

--- Pyt_2/7. Hafta/Döngüler.py
+++ Pyt_2/7. Hafta/Döngüler.py
@@ -31,25 +31,78 @@ print(sonuc)
 
 """
 # Fibonacci 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 
-
-terim_sayisi = 1000
-
-a = 0
-b = 1
-
-c = 0
-
-for i in range(terim_sayisi):
-    c = a+b
-    a = b
-    b = c
-    print(c)
-
-
-input()
-
+##
+##terim_sayisi = 1000
+##
+##a = 0
+##b = 1
+##
+##c = 0
+##
+##for i in range(terim_sayisi):
+##    c = a+b
+##    a = b
+##    b = c
+##    print(c)
+##
+##input()
 
 
+# While => iken
 
+##kosul = True
+##sayac = 0
+##
+##while sayac < 50:
+##    sayac += 1
+##    print(sayac)
+
+##    if sayac == 50:
+##        kosul = False
+
+##sayac = 48
+##
+##while sayac < 112:
+##    sayac += 4
+##    print(sayac)
+
+##while True:
+##    
+##    a = """
+##    İŞLEMLER
+##    1- Toplama
+##    2- Çıkarma
+##    3- Çarpma
+##    4- Bölme
+##    q- Çıkış
+##    """
+##    
+##    print(a)
+##    sec = input("Lütfen Bir İşlem Seçiniz:")
+##
+##    if sec == "q":
+##        break
+##
+##    s1 = float(input("1. Sayıyı Giriniz:"))
+##    s2 = float(input("2. Sayıyı Giriniz:"))
+##
+##    if sec == "1":
+##        print("İki Sayının Toplamı =>", s1+s2)
+##    elif sec == "2":
+##        print("İki Sayının Farkı =>", s1-s2)
+##    elif sec == "3":
+##        print("İki Sayını Çarpımı =>", s1*s2)
+##    elif sec == "4":
+##        print("İki Sayının Bölümü =>",s1/s2)
+##
+
+sayac = 0
+while sayac < 50:
+    sayac += 1 
+
+    if sayac %5 == 0:
+        continue
+
+    print(sayac)
 
 
