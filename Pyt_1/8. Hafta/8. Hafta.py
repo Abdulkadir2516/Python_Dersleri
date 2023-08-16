a351c41295bbc976acdefc7f42263640c88048aa
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2023-08-16 20:42:15 +0300

    pyt-2 8. hafta 1. Ders

--- Pyt_1/8. Hafta/8. Hafta.py
+++ Pyt_1/8. Hafta/8. Hafta.py
@@ -17,12 +17,12 @@
 ##second = 1
 ##result = 0
 ##
-##x = 251567489
+####x = 251567489
 ##print(1, ". terimi =>" ,first)
 ##print(2, ". terimi =>" ,second)
 ##
 ##sayac = 3
-##while result < x:
+##while sayac < 30:
 ##    result = first + second
 ##    first = second
 ##    second = result
@@ -30,31 +30,52 @@
 ##    sayac += 1
 
 
-first = 0
-second = 1
-result = 0
+##fibs = [0,1]
+##
+##sayac = 0
+##while sayac < 30:
+##    result = fibs[sayac] + fibs[sayac+1]
+##    print(result)
+##    fibs.append(result)
+##    sayac +=1
+##    print(fibs)
 
-x = 251567489
-print(1, ". terimi =>" ,first)
-print(2, ". terimi =>" ,second)
+##
+##first = 0
+##second = 1
+##result = 0
+##
+##x = 251567489
+##print(1, ". terimi =>" ,first)
+##print(2, ". terimi =>" ,second)
+##
+##sayac = 3
+##while True:
+##    result = first + second
+##    first = second
+##    second = result
+##
+##    if sayac == 15:
+##        sayac +=1
+##        continue
+##    
+##    print(sayac , ". terimi =>" ,result)
+##    sayac += 1
+##
+##    if result > x :
+##        break
 
-sayac = 3
-while True:
-    result = first + second
-    first = second
-    second = result
 
-    if sayac == 15:
-        sayac +=1
-        continue
+
+sayac = 0
+
+while sayac < 5:
+    sayac +=1
+    print(sayac)
+
+else:
+    print("hello")
     
-    print(sayac , ". terimi =>" ,result)
-    sayac += 1
-
-    if result > x :
-        break
-    
-
 
 
 
---
+++ Pyt_2/8. Hafta/Döngüler.py
@@ -0,0 +1,61 @@
+# DÖNGÜLER
+# LİSTELER
+
+##a = 0
+##while a < 5:
+##    a += 1
+##    print(a)
+##else:
+##    print(a)
+
+# Koleksiyonlar => Listeler (List), Demetler (Tuple), Kümeler (Set), Sözlük (Dict)
+##
+##liste = ["deneme2"]
+##print(liste)
+##print(type(liste))
+##liste = list(["deneme"])
+##print(liste)
+
+
+##fruits = ["Elma","Armut","Karpuz","Kavun","Mandalina","Domates","Şeftali"]
+##asallar = [2,3,5,7,11,17,19,23,29,31,37,41,43]
+##
+##print(fruits)
+##print(asallar)
+##
+##
+##print(fruits[3])
+##print(asallar[3])
+##print(asallar[-3])
+##
+##düzenli_liste = list(range(10,100,7))
+##print(düzenli_liste)
+##
+##print(fruits[5:10])
+##print(asallar[5:10])
+##
+##print(fruits[5:10:2])
+##print(asallar[5:10:2])
+##print(düzenli_liste[5:20:2])
+##
+##
+##print(asallar[-5])
+##print(asallar[-10:-3])
+##
+##print(düzenli_liste[::-1])
+##
+##print(len(asallar))
+
+fruits = ["Elma","Armut","Karpuz","Kavun","Mandalina","Domates","Şeftali"]
+asallar = [2,3,5,7,11,17,19,23,29,31,37,41,43]
+
+fruits += ["Kayısı"]
+
+fruits[2] ="Erik"
+
+print(fruits)
+
+
+
+
+
